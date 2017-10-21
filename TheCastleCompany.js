af385087a28721b15015cdfdfa16f8cd0cf49781
harleycarr <harley.carr@gmail.com>
2017-10-21 10:37:36 -0700

    make more terse

--- TheCastleCompany.js
+++ TheCastleCompany.js
@@ -1,6 +1,6 @@
-var geography = [1,1,2,3,3,4,3,2,2,1,1,2,1,4];
-
 function placeCastles(geography){
+  var castles = 0;
+  var direction = 0;
   // takes in array of integers, find true peaks and values. returns count.
   
   function checkDirection(index){
@@ -13,7 +13,7 @@ function placeCastles(geography){
       direction = -1;
     }
 
-    // check for directionChange
+    // check for direction change
     if(geography[index] < geography[index+1] && direction < 0){
       changeDirection = true
     } else if(descending = geography[index] > geography[index+1] && direction > 0){
@@ -23,25 +23,13 @@ function placeCastles(geography){
     return changeDirection;
   }
 
-  var castles = 0;
-  var direction = 0;
-
-  // place initial castle
+  // count castles
   if(geography.length) castles++;
-
-  // iterate geography
   geography.forEach(function(item, index){
-    var changeDirection = checkDirection(index);
-    if(changeDirection){
-      castles++
-    }
-
-    // place final castle
-    if(typeof geography[index+1] == 'undefined' && (direction !== 0)){
-      castles++;
-    }
+    if(checkDirection(index)) castles++
+    if(typeof geography[index+1] == 'undefined' && (direction !== 0)) castles++;
   });
   return castles;
 }
 
-console.log('castles: ', placeCastles(geography));
+console.log('castles: ', placeCastles([1,1,2,3,3,4,3,2,2,1,1,2,1,4])); // 6 castles in mock.
