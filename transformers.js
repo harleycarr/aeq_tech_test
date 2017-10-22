ded70f742435bf62fb405cb1dcb24b62cace24fc
harleycarr <harley.carr@gmail.com>
2017-10-21 17:27:48 -0700

    refactor for maintainability

--- transformers.js
+++ transformers.js
@@ -1,6 +1,7 @@
 battles = 0;
 
 function Transformer (name, stats) {
+  // Probably should have included a .fight method, this class is pretty classless.
   this.name = name;
   this.stats = stats;
   this.rating = stats.strength + stats.intelligence + stats.speed + stats.endurance + stats.firepower;
@@ -30,45 +31,25 @@ function teamBattle(teamA,teamB) {
   function bosses(a,b) {
     return a.isBoss && b.isBoss;
   }
-  function boss(a,b) {
-    if(a.isBoss){
+
+  // checks who wins various checks
+  function whoIsBetter(check, a, b){
+    if(check(a,b)) {
       b.active = false;
       return true;
-    } else if(b.isBoss){
+    } else if (check(b,a)){
       a.active = false;
-      return true;
+      return true
     }
     return false;
   }
 
-  function flee(a,b) {
-    if (a.stats.courage - b.stats.courage >= 4 && a.stats.strength - b.stats.strength >= 3) {
-      b.active = false;
-      return true;
-    } else if (b.stats.courage - a.stats.courage >= 4 && b.stats.strength - a.stats.strength >= 3) {
-      a.active = false;
-      return true;
-    } else return false;
-  }
+  // Expressions for predicate checks on whoIsBetter
+  function boss(a,b)    { return a.isBoss };
+  function courage(a,b) { return a.stats.courage - b.stats.courage >= 4 && a.stats.strength - b.stats.strength >= 3 };
+  function skill(a,b)   { return a.stats.skill - b.stats.skill >= 3 };
+  function rating(a,b)  { return a.rating > b.rating };
 
-  function skill(a,b){
-    if (a.stats.skill - b.stats.skill >= 3) {
-      b.active = false;
-      return true;
-    } else if (b.stats.skill - a.stats.skill >= 3) {
-      a.active = false;
-      return true;
-    } else return false;
-  }
-
-  function rating(a,b){
-    if (a.rating > b.rating){
-      b.active = false;
-    } else if (b.rating > a.rating){
-      a.active = false;
-    }
-    return true;
-  }
   battles = teamA.length < teamB.length ? autobots.length : decepticons.length;
 
   for(i=0; i < battles; i++){
@@ -78,9 +59,8 @@ function teamBattle(teamA,teamB) {
       break;
     }
 
-    if([boss,flee,skill,rating, function(a,b){a.active = false; b.active = false;}]
-      .some(function(check){return check(teamA[i], teamB[i])}))
-    continue;
+    [boss,courage,skill,rating, function(a,b){a.active = false; b.active = false;}]
+      .some(function(check){return whoIsBetter(check, teamA[i], teamB[i])});
   }
 }
 
